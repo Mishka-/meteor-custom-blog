050c0fd0f13d22b4e0cc011abe68d9e1aad386b1
Michael <hommi@tut.by>
2016-04-13 19:20:27 +0200

    blog added

---
+++ client/common.js
@@ -0,0 +1,26 @@
+Meteor.subscribe("posts");
+
+Meteor.subscribe("ProfileImages");
+
+Meteor.subscribe("UserImages");
+
+Meteor.startup(function () {
+    AccountsEntry.config({
+
+        homeRoute: '/',
+        dashboardRoute: '/',
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+
+
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+    })
+});
+
+Template.registerHelper('getProfileImg', function(userId){
+    var imgUrl = UserImages.findOne({userId: userId}).image;
+    console.log(imgUrl);
+    return imgUrl;
+});
